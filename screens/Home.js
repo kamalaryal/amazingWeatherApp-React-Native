575a6366fff64e2630e58589956f55483f9990e0
aryalkamal201 <aryalkamal201@gmail.com>
2020-04-25 19:43:58 +0545

    final deploy

---
+++ screens/Home.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { Card, Title } from 'react-native-paper';
+import { View, Image } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import Header from './Header';
+
+export default Home = (props) => {
+    const [info, setInfo] = useState({
+        name: "loading !!",
+        temp: "loading",
+        humidity: "loading",
+        desc: "loading",
+        icon: "loading"
+    });
+    useEffect(() => {
+        getWeather();
+    }, [])
+    const getWeather = async () => {
+        let MyCity = await AsyncStorage.getItem('newcity');
+        if (!MyCity) {
+            const { city } = props.route.params;
+            MyCity = city;
+        }
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${MyCity}&APPID=73c01db48729370143647863028e89dd&units=metric`)
+        .then(data=>data.json())
+        .then(results=>{
+           setInfo({
+               name:results.name,
+               temp:results.main.temp,
+               humidity:results.main.humidity,
+               desc:results.weather[0].description,
+               icon:results.weather[0].icon,
+           })
+        })
+        .catch(err => {
+            alert(err.message);
+        });
+    }
+    if (props.route.params.city != "london") {
+        getWeather();
+    }
+    return (
+        <View style={{flex: 1}}>
+            <Header name='Weather App'  />
+            <View style={{alignItems:"center"}}>
+                <Title style={{ color: '#00aaff', marginTop: 30, fontSize: 30 }}>
+                    {info.name}
+                </Title>
+                <Image style={{ width: 120, height: 120 }} source={{ uri: `https://openweathermap.org/img/w/${info.icon}.png`}} />
+            </View>
+            <Card style={{ margin: 5, padding: 12 }}>
+                <Title style={{ color: '#00aaff' }}>Temperature - {info.temp}</Title>
+            </Card>
+            <Card style={{ margin: 5, padding: 12 }}>
+                <Title style={{ color: '#00aaff' }}>Humidity - {info.humidity}</Title>
+            </Card>
+            <Card style={{ margin: 5, padding: 12 }}>
+                <Title style={{color:"#00aaff"}}>Description-  {info.desc}</Title>
+            </Card>
+        </View>
+    )
+}
